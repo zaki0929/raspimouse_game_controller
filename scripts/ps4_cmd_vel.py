19e3a72abf37a1236a9f63ba8c49e31326c0e20a
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2017-06-02 20:26:22 +0900

    Add scripts for use a controller

---
+++ scripts/ps4_cmd_vel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger, TriggerResponse
+from sensor_msgs.msg import Joy
+
+class JoyTwist(object):
+    def __init__(self):
+        self._joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback, queue_size=1)
+        self._twist_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+
+    def joy_callback(self, joy_msg):
+        if joy_msg.buttons[0] == 1:
+            twist = Twist()
+            twist.linear.x = joy_msg.axes[7] * 0.2
+            twist.angular.z = joy_msg.axes[6] * 3.14/4
+            self._twist_pub.publish(twist)
+
+if __name__ == '__main__':
+    rospy.wait_for_service('/motor_on')
+    rospy.wait_for_service('/motor_off')
+    rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
+    rospy.ServiceProxy('/motor_on', Trigger).call()
+    rospy.init_node('ps4_cmd_vel')
+    ps4_cmd_vel = JoyTwist()
+    rospy.spin()
